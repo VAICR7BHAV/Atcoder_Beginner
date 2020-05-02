b5b42a6d87e65ba12637506298db0fece485d3eb
vaibhav gautam <vaibhav.gautam2308@gmail.com>
2020-05-03 00:54:09 +0530

    Create A. CirclePond.cpp

---
+++ ABC163/A. CirclePond.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+# define M_PI 3.14159265358979323846
+using namespace std;
+using ll = long long int;
+const int MOD = 1e9 + 7;
+int main()
+{
+    int r;
+    cin>>r;
+    cout<<2*M_PI*r;
+}
